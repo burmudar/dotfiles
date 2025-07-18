55744079f640470bfb93b5b0351d538ae78e5a2b
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2025-07-18 16:37:38 +0200

    snippets

--- vim/lua/plugins/completion.lua
+++ vim/lua/plugins/completion.lua
@@ -1,13 +1,14 @@
 return {
   {
     'saghen/blink.cmp',
-    dependencies = 'rafamadriz/friendly-snippets',
+    dependencies = { 'rafamadriz/friendly-snippets', { 'L3MON4D3/LuaSnip', version = 'v2.*' } },
 
     version = '*',
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
       keymap = { preset = 'default' },
+      snippets = { preset = 'luasnip' },
       appearance = {
         -- sets the fallback highlight groups to nvim-cmp's highlight groups
         -- useful for when your theme doesn't support blink.cmp
@@ -22,24 +23,7 @@ return {
         documentation = { auto_show = true, auto_show_delay_ms = 500 },
       },
     },
-      -- experimental signature help support
-      signature = { enabled = false },
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    enabled = false,
-    opts = function(_, opts)
-      opts.sources = opts.sources or {}
-      table.insert(opts.sources, {
-        name = "lazydev",
-        group_index = 0, -- set group index to 0 to skip loading LuaLS completions
-      })
-    end,
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-cmdline",
-    }
+    -- experimental signature help support
+    signature = { enabled = false },
   },
 }
--- vim/lua/snippets/norg.lua
+++ vim/lua/snippets/norg.lua
@@ -14,7 +14,7 @@ ls.add_snippets("norg", {
   s("daily", {
     t({ "* Journal — " }), f(date), t({ "", "" }),
 
-    t({ "**** 🌇 Evening Wrap", "" }),
+    t({ "** 🌇 Evening Wrap", "" }),
     t({ "- 🛠 What did I actually work on?", "  • " }), i(1), t({ "", "" }),
     t({ "- ⚡ What had momentum or felt satisfying?", "  • " }), i(2), t({ "", "" }),
     t({ "- 🧱 What pulled energy, frustrated, or felt heavy?", "  • " }), i(3), t({ "", "" }),
@@ -30,12 +30,20 @@ ls.add_snippets("norg", {
     t({ "- 🚩 Any assumptions to challenge?", "  • " }), i(11), t({ "", "" }),
     t({ "- ⏭️ Next step or experiment:", "  • " }), i(12), t({ "", "" }),
 
-    t({ "", "**** 💡 Interesting Things", "- " }), i(13), t({ "", "- " }), i(14), t({ "", "- " }), i(15), t({ "", "" }),
+    t({ "", "*** 💡 Interesting Things", "- " }), i(13), t({ "", "- " }), i(14), t({ "", "- " }), i(15), t({ "", "" }),
 
-    t({ "**** 📍 One-liner Insight of the Day", "- " }), i(16),
+    t({ "*** 📍 One-liner Insight of the Day", "- " }), i(16),
+  }),
+  s("morning", {
+    t({ "** 🌞 Morning Kickstart", "" }),
+    t({ "- 🧠 How do I want to *feel* today?", "  • " }), i(1), t({ "", "" }),
+    t({ "- 🎯 What’s my *one key intention* or focus?", "  • " }), i(2), t({ "", "" }),
+    t({ "- 🌱 One habit I want to keep or shift today:", "  • " }), i(3), t({ "", "" }),
+    t({ "- 🚧 Anything that might get in the way?", "  • " }), i(4), t({ "", "" }),
+    t({ "- ✅ What will make today feel like a win?", "  • " }), i(5),
   }),
   s("evening", {
-    t({ "**** 🌇 Evening Wrap", "" }),
+    t({ "** 🌇 Evening Wrap", "" }),
     t({ "- 🛠 What did I actually work on?", "  • " }), i(1), t({ "", "" }),
     t({ "- ⚡ What had momentum or felt satisfying?", "  • " }), i(2), t({ "", "" }),
     t({ "- 🧱 What pulled energy, frustrated, or felt heavy?", "  • " }), i(3), t({ "", "" }),
@@ -44,7 +52,7 @@ ls.add_snippets("norg", {
     t({ "- 🔄 Do I want to change how I show up tomorrow?", "  • " }), i(6), t({ "", "" }),
   }),
   s("work", {
-    t({ "", "**** 🛠 Work Debug", "" }),
+    t({ "", "** 🛠 Work Debug", "" }),
     t({ "- 🔧 What am I working on?", "  • " }), i(7), t({ "", "" }),
     t({ "- 🔍 What's not clear or causing friction?", "  • " }), i(8), t({ "", "" }),
     t({ "- 🧠 Any ideas or hunches?", "  • " }), i(9), t({ "", "" }),
