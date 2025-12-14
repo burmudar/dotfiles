e9fd399d30bd4f4d681b29fa32b1182cc2dd489a
William Bezuidenhout <william.bezuidenhout@gmail.com>
2025-12-14 10:06:19 +0200

    extra hardware modules

--- nix/hosts/desktop/hardware-configuration.nix
+++ nix/hosts/desktop/hardware-configuration.nix
@@ -9,7 +9,7 @@
       (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" "jc42" "nct6775"];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
