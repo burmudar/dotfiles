2e8a04233314bd5b7c475aada82a968518ce2464
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2026-04-10 21:35:27 +0200

    vim formatting

--- vim/lua/burm/autocmd.lua
+++ vim/lua/burm/autocmd.lua
@@ -1,25 +1,25 @@
 local M = {}
 
 local function registerAutoCmd()
-  vim.api.nvim_create_autocmd("FileType", {
-    pattern = { "yaml", "yml" },
-    callback = function()
-      vim.opt_local.expandtab = true
-      vim.opt_local.shiftwidth = 2
-      vim.opt_local.tabstop = 2
-    end,
-  })
-  --- Remove trailing whitespace before write
-  vim.cmd([[
+	vim.api.nvim_create_autocmd("FileType", {
+		pattern = { "yaml", "yml" },
+		callback = function()
+			vim.opt_local.expandtab = true
+			vim.opt_local.shiftwidth = 2
+			vim.opt_local.tabstop = 2
+		end,
+	})
+	--- Remove trailing whitespace before write
+	vim.cmd([[
     augroup BURM_WHITESPACE
         autocmd!
         autocmd BufWritePre * %s/\s\+$//e
     augroup END
     ]])
 
-  -- Formatting is now handled by conform.nvim (see plugins/conform.lua)
+	-- Formatting is now handled by conform.nvim (see plugins/conform.lua)
 
-  vim.cmd([[
+	vim.cmd([[
     augroup BURM_GOLANG
       autocmd!
 
@@ -29,13 +29,13 @@ local function registerAutoCmd()
     augroup END
     ]])
 
-  vim.cmd([[
+	vim.cmd([[
     augroup BURN_PYTHON
         autocmd BufNewFile,BufRead *.py set tabstop=4 softtabstop=4 shiftwidth=4 expandtab smarttab autoindent
     augroup END
     ]])
 
-  vim.cmd([[
+	vim.cmd([[
     augroup RUST
         autocmd BufNewFile,BufRead *.rs nnoremap <F5> :RustRun<CR>
     augroup END
@@ -43,82 +43,82 @@ local function registerAutoCmd()
 end
 
 M.setup = function()
-  registerAutoCmd()
+	registerAutoCmd()
 
-  -- register user command to disable/enable auto-formatting
-  vim.api.nvim_create_user_command("FormatDisable", function()
-    vim.b.disable_autoformat = true
-    print("Auto-format disabled for this buffer")
-  end, {})
+	-- register user command to disable/enable auto-formatting
+	vim.api.nvim_create_user_command("FormatDisable", function()
+		vim.b.disable_autoformat = true
+		print("Auto-format disabled for this buffer")
+	end, {})
 
-  vim.api.nvim_create_user_command("FormatEnable", function()
-    vim.b.disable_autoformat = false
-    print("Auto-format enabled for this buffer")
-  end, {})
+	vim.api.nvim_create_user_command("FormatEnable", function()
+		vim.b.disable_autoformat = false
+		print("Auto-format enabled for this buffer")
+	end, {})
 
-  -- register a user command which can format json using jq
-  vim.api.nvim_create_user_command("JSONF", "% !jq", {})
+	-- register a user command which can format json using jq
+	vim.api.nvim_create_user_command("JSONF", "% !jq", {})
 
-  -- register user command with !! which puts the entire buffer through a shell command
-  vim.api.nvim_create_user_command("RR", function(opts)
-    local range = ""
-    if opts.range > 0 then
-      range = string.format(":%d,%d!", opts.line1, opts.line2)
-    else
-      range = ":%!"
-    end
-    vim.cmd(range .. opts.args)
-  end, {
-    nargs = "+",
-    range = true,
-    desc = "Pipe (pipe of the buffer through a shell command"
-  })
+	-- register user command with !! which puts the entire buffer through a shell command
+	vim.api.nvim_create_user_command("RR", function(opts)
+		local range = ""
+		if opts.range > 0 then
+			range = string.format(":%d,%d!", opts.line1, opts.line2)
+		else
+			range = ":%!"
+		end
+		vim.cmd(range .. opts.args)
+	end, {
+		nargs = "+",
+		range = true,
+		desc = "Pipe (pipe of the buffer through a shell command",
+	})
 
-  -- register a user command to insert co-author lines
-  vim.api.nvim_create_user_command("Coauth", function(opts)
-    local keegan = "Co-authored-by Keegan Carruthers-Smith <keegan.csmith@gmail.com>"
-    local bolaji = "Co-authored-by Bolaji Olajide <25608335+BolajiOlajide@users.noreply.github.com>"
+	-- register a user command to insert co-author lines
+	vim.api.nvim_create_user_command("Coauth", function(opts)
+		local keegan = "Co-authored-by Keegan Carruthers-Smith <keegan.csmith@gmail.com>"
+		local bolaji = "Co-authored-by Bolaji Olajide <25608335+BolajiOlajide@users.noreply.github.com>"
 
-    local text = ""
-    if opts.args == "keegan" then
-      text = keegan
-    elseif opts.args == "bolaji" then
-      text = bolaji
-    else
-      text = keegan .. "\n" .. bolaji
-    end
+		local text = ""
+		if opts.args == "keegan" then
+			text = keegan
+		elseif opts.args == "bolaji" then
+			text = bolaji
+		else
+			text = keegan .. "\n" .. bolaji
+		end
 
-    -- Insert the text at the current cursor position
-    local pos = vim.api.nvim_win_get_cursor(0)
-    local line = pos[1] - 1
-    vim.api.nvim_buf_set_lines(0, line, line, false, vim.split(text, "\n"))
-  end, {
-    nargs = "?",
-    desc = "Insert co-author lines for commit messages"
-  })
-  -- Show errors and warnings in a floating window
-  vim.api.nvim_create_autocmd("CursorHold", {
-    callback = function()
-      for _, win in ipairs(vim.api.nvim_list_wins()) do
-        local config = vim.api.nvim_win_get_config(win)
-        if config.relative ~= "" then
-          return
-        end
-      end
-      vim.diagnostic.open_float(nil, { focusable = false, source = "if_many" })
-    end,
-  })
+		-- Insert the text at the current cursor position
+		local pos = vim.api.nvim_win_get_cursor(0)
+		local line = pos[1] - 1
+		vim.api.nvim_buf_set_lines(0, line, line, false, vim.split(text, "\n"))
+	end, {
+		nargs = "?",
+		desc = "Insert co-author lines for commit messages",
+	})
+	-- Show errors and warnings in a floating window
+	vim.api.nvim_create_autocmd("CursorHold", {
+		callback = function()
+			for _, win in ipairs(vim.api.nvim_list_wins()) do
+				local config = vim.api.nvim_win_get_config(win)
+				if config.relative ~= "" then
+					return
+				end
+			end
+			vim.diagnostic.open_float(nil, { focusable = false, source = "if_many" })
+		end,
+	})
 
-  -- Inspect floats
-  vim.api.nvim_create_user_command("InspectFloats", function()
-    for _, win in ipairs(vim.api.nvim_list_wins()) do
-      local config = vim.api.nvim_win_get_config(win)
-      if config.relative ~= "" then
-        print("Float window: ", win)
-        print(vim.inspect(config))
-      end
-    end
-  end, {})
+	-- Inspect floats
+	vim.api.nvim_create_user_command("InspectFloats", function()
+		for _, win in ipairs(vim.api.nvim_list_wins()) do
+			local config = vim.api.nvim_win_get_config(win)
+			if config.relative ~= "" then
+				print("Float window: ", win)
+				print(vim.inspect(config))
+			end
+		end
+	end, {})
 end
 
 return M
