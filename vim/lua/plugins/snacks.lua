83f4fa8bc901dd3c6726cbe26b92956c3ef3e7ba
William Bezuidenhout <william.bezuidenhout@gmail.com>
2026-07-07 22:08:11 +0200

    fix vim config for Neogit

--- vim/lua/plugins/snacks.lua
+++ vim/lua/plugins/snacks.lua
@@ -102,7 +102,7 @@ return {
     { "<leader>uC",      function() Snacks.picker.colorschemes() end,                            desc = "Colorschemes" },
     -- LSP
     { "gd",              function() Snacks.picker.lsp_definitions() end,                         desc = "Goto Definition" },
-    { "<M-d>",           definition_vsplit,                                                   desc = "Definition in vsplit" },
+    { "<M-d>",           definition_vsplit,                                                      desc = "Definition in vsplit" },
     { "gD",              function() Snacks.picker.lsp_declarations() end,                        desc = "Goto Declaration" },
     { "gr",              function() Snacks.picker.lsp_references() end,                          nowait = true,                     desc = "References" },
     { "gI",              function() Snacks.picker.lsp_implementations() end,                     desc = "Goto Implementation" },
@@ -118,7 +118,6 @@ return {
     { "<leader>bd",      function() Snacks.bufdelete() end,                                      desc = "Delete Buffer" },
     { "<leader>cR",      function() Snacks.rename.rename_file() end,                             desc = "Rename File" },
     { "<leader>gB",      function() Snacks.gitbrowse() end,                                      desc = "Git Browse",               mode = { "n", "v" } },
-    { "<leader>gg",      function() Snacks.lazygit() end,                                        desc = "Lazygit" },
     { "<leader>un",      function() Snacks.notifier.hide() end,                                  desc = "Dismiss All Notifications" },
     { "<c-/>",           function() Snacks.terminal() end,                                       desc = "Toggle Terminal" },
     { "<c-_>",           function() Snacks.terminal() end,                                       desc = "which_key_ignore" },
