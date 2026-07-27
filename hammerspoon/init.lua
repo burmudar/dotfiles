cd5ac6529973f46cc1007bb3098c276af4fd7430
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2026-07-27 10:59:46 +0200

    add vivaldi as binding in hammperspoon

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,54 +1,54 @@
 require("burm.watcher").setup({
-  bind = {
-    meta = { "cmd" },
-    key = "`"
-  }
+	bind = {
+		meta = { "cmd" },
+		key = "`",
+	},
 })
 
 require("burm.windows").setup({
-  show = {
-    {
-      meta = { "cmd" },
-      key = "1",
-      apps = { "ghostty", "kitty", "alacritty" }
-    },
-    {
-      meta = { "cmd" },
-      key = "2",
-      fn = function()
-        local zoom = hs.application.find("zoom")
-        if zoom then
-          local win = zoom:findWindow("Zoom Meeting")
-          win:focus()
-          return
-        end
-      end
-    },
-    {
-      meta = { "cmd" },
-      key = "3",
-      apps = "slack"
-    },
-    {
-      meta = { "cmd" },
-      key = "4",
-      apps = { "Firefox", "qutebrowser" }
-    },
-  },
-  move = {
-    {
-      meta = { "cmd", "shift" },
-      key = "1",
-      window = 1
-    },
-    {
-      meta = { "cmd", "shift" },
-      key = "2",
-      window = 2
-    }
-  },
-  maximise = {
-    meta = { "cmd", "shift" },
-    key = "w",
-  },
+	show = {
+		{
+			meta = { "cmd" },
+			key = "1",
+			apps = { "ghostty", "kitty", "alacritty" },
+		},
+		{
+			meta = { "cmd" },
+			key = "2",
+			fn = function()
+				local zoom = hs.application.find("zoom")
+				if zoom then
+					local win = zoom:findWindow("Zoom Meeting")
+					win:focus()
+					return
+				end
+			end,
+		},
+		{
+			meta = { "cmd" },
+			key = "3",
+			apps = "slack",
+		},
+		{
+			meta = { "cmd" },
+			key = "4",
+			apps = { "Vivaldi", "qutebrowser" },
+		},
+	},
+	move = {
+		{
+			meta = { "cmd", "shift" },
+			key = "1",
+			window = 1,
+		},
+		{
+			meta = { "cmd", "shift" },
+			key = "2",
+			window = 2,
+		},
+	},
+	maximise = {
+		meta = { "cmd", "shift" },
+		key = "w",
+	},
 })
